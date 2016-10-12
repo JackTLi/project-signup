883e761a2e332e743cdabfad0604dfab2e98bedb
JackTLi <jack.teng.li@gmail.com>
2016-10-11 21:23:48 -0400

    retain list of users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,14 @@
 class User < ActiveRecord::Base
-	has_and_belongs_to_many :homes
 
-	def self.from_omniauth(auth)
-		where(auth.slice(provider: auth.provider, uid: auth.uid)).first_or_initialize.tap do |user|
+  def self.from_omniauth(auth)
+		where(auth.slice(provider: auth.provider, uid: auth.uid)).find_or_create_by(uid: auth.uid) do |user|
 			user.provider = auth.provider
 			user.uid = auth.uid
-			user.email = auth.email
 			user.name = auth.info.name
 			user.image = auth.info.image
 			user.oauth_token = auth.credentials.token
 			user.oauth_expires_at = Time.at(auth.credentials.expires_at)
 			user.save!
 		end
-	end
+  end
 end
