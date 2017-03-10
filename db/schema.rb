f84813139467ec520823d928fcb6f204b462e765
Matthew Black <mblackmblackmblack@gmail.com>
2017-03-10 16:28:59 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161012003123) do
+ActiveRecord::Schema.define(version: 20170310211642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string   "url"
+    t.integer  "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.text     "body_html"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "pid"
+    t.string   "tags"
+    t.string   "title"
+    t.string   "vendor"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "provider"
