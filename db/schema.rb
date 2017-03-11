c256b09b323472c421549a4bce66435db9481bcd
Matthew Black <mblackmblackmblack@gmail.com>
2017-03-11 15:05:30 -0500

    fix carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -48,14 +48,6 @@ ActiveRecord::Schema.define(version: 20170311193110) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "records", force: :cascade do |t|
-    t.string   "title"
-    t.date     "date"
-    t.float    "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
